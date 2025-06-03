81aee75c002bdeffd27a42b70e95092d24784022
bairagi2oo3 <tusharbairage95@gmail.com>
2025-06-03 22:13:53 +0530

    Update userbot.py

--- JioSavaan/core/userbot.py
+++ JioSavaan/core/userbot.py
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
+                await self.four.join_chat("THE_LINK_ADDA")
+                await self.four.join_chat("KING_BOT_CHANNEL")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("FallenAssociation")
+                await self.five.join_chat("THE_LINK_ADDA")
+                await self.five.join_chat("KING_BOT_CHANNEL")
             except:
                 pass
             assistants.append(5)
